560cf6b92346363db0a45e7c0e26372e150d4919
sylotana <anatolyshangin@mail.ru>
2023-12-05 22:27:35 +0300

    Добавил библиотеки для препроцессинга

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,11 @@ const concatCSS = require('gulp-concat-css'); // подключаем gulp-conca
 const plumber = require('gulp-plumber'); // подключаем gulp-plumber
 const del = require('del'); // подключаем del
 const browserSync = require('browser-sync').create(); // подключаем browser-sync
+const postcss = require('gulp-postcss'); // подключаем gulp-postcss
+const autoprefixer = require('autoprefixer'); // подключаем autoprefixer
+const mediaquery = require('postcss-combine-media-query') // подключаем postcss-combine-media-query
+const cssnano = require('cssnano') // подключаем cssnano
+const htmlMinify = require('html-minifier') // подключаем html-minifier
 
 
 
@@ -26,28 +31,61 @@ const browserSync = require('browser-sync').create(); // подключаем br
  */
 
 function html() {
+  const options = {
+    /**
+     * Настройки для плагина html-minifer
+     */
+    removeComments: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    sortClassName: true,
+    useShortDoctype: true,
+    collapseWhitespace: true,
+    minifyCSS: true,
+    keepClosingSlash: true
+  };
   return gulp.src('src/**/*.html')
     .pipe(plumber())
-    .pipe(gulp.dest('dist/'))
+    .on('data', function(file) {
+      const buferFile = Buffer.from(htmlMinify.minify(file.contents.toString(), options))
+      return file.contents = buferFile
+    })
+    .pipe(gulp.dest('build/'))
     .pipe(browserSync.reload({stream: true}));
 }
 
 function css() {
+  const plugins = [
+    /**
+     * autoprefixer - этот плагин смотрит browserslist внутри package.json
+     * и подставляет вендорные префиксы для браузеров, которым они нужны
+     * 
+     * mediaquery (postcss-combine-media-query) - находит все медиазапросы
+     * с одинаковым параметрами в исходниках и склеивает их в один медиазапрос при сборке
+     * 
+     * cssnano - минификация сборки
+     */
+    autoprefixer(),
+    mediaquery(),
+    cssnano()
+  ]
   return gulp.src('src/**/*.css')
     .pipe(plumber())
     .pipe(concatCSS('bundle.css'))
-    .pipe(gulp.dest('dist/'))
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('build/'))
     .pipe(browserSync.reload({stream: true}));
 }
 
 function assets() {
   return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
-    .pipe(gulp.dest('dist/images'))
+    .pipe(gulp.dest('build/images'))
     .pipe(browserSync.reload({stream: true}));
 }
 
 function clean() {
-  return del('dist');
+  return del('build');
 }
 
 function watchFiles() {
@@ -59,7 +97,7 @@ function watchFiles() {
 function serve() {
   browserSync.init({
     server: {
-      baseDir: './dist'
+      baseDir: './build'
     }
   });
 }
@@ -78,13 +116,13 @@ function serve() {
  */
 
 /**
- * gulp html - переносит все html-файлы из src/ в dist/
- * gulp css - склеивает все css-файлы в bundle.css и переносит из src/ в dist/
- * gulp assets - переносит все изображения из src/images в dist/images
- * gulp clean - удаляет папку dist/
+ * gulp html - переносит все html-файлы из src/ в build/
+ * gulp css - склеивает все css-файлы в bundle.css и переносит из src/ в build/
+ * gulp assets - переносит все изображения из src/images в build/images
+ * gulp clean - удаляет папку build/
  * 
- * gulp build - сперва выполняет удаление dist/, а потом преобразование
- * и файлов из src/ в новую (очищенную) dist/
+ * gulp build - сперва выполняет удаление build/, а потом преобразование
+ * и файлов из src/ в новую (очищенную) build/
  * 
  * gulp watchApp - следит за файлами в src/ и делает пересборку
  * после каждого изменения этих файлов
